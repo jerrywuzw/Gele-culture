52014f6af670bdd6590b8cfa8136273cf367728d
zwu3 <99296226+zwu3@users.noreply.github.com>
2025-01-22 02:46:55 -0800

    vitr.config and index.html fix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/Gele-culture/', // Set the correct base for GitHub Pages
   plugins: [react()],
-  base: '/Gele-culture/', // Match the repository name
-  build: {
-    outDir: 'dist', // Ensure this matches the deploy directory
-  },
 });
