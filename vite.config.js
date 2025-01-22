efcbdb998bfd0674afe32967efb19043b98bb629
zwu3 <99296226+zwu3@users.noreply.github.com>
2025-01-22 03:30:44 -0800

    f

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   base: '/Gele-culture/', // Set the correct base for GitHub Pages
   plugins: [react()],
+  build: {
+    assetsInlineLimit: 0,
+  },
 });
