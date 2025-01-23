a8acfe7acccb70c63db380a9e45b577841eb4f0f
zwu3 <99296226+zwu3@users.noreply.github.com>
2025-01-22 20:44:54 -0800

    reroute out of repo address

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,13 @@ import react from '@vitejs/plugin-react-swc';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  // If you're deploying to /Gele-culture/, keep this; otherwise remove it.
-  base: '/Gele-culture/',
+  base: '/', // Use '/' for custom domains
   plugins: [
     react(),
     createHtmlPlugin({
       inject: {
         injectData: {
-          title: 'Gele.Culture'
+          title: 'Gele'
         }
       }
     })
